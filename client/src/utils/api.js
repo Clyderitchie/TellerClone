17aeb785c334e0fdede96d9e503c9c6652dfbc9e
francis <clyderitchie@yahoo.com>
2024-01-27 22:08:29 -0800

    Added all the api routes for client, account, teller

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,6 @@
-export const getAllMatchups = () => {
-  return fetch('/api/matchup', {
+// Client
+export const getAllClients = () => {
+  return fetch('/api/client', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -7,40 +8,79 @@ export const getAllMatchups = () => {
   });
 };
 
-export const createMatchup = (matchupData) => {
+export const getClient = (clientId) => {
+  return fetch(`/api/client/${clientId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const createClient = (clientData) => {
+  return fetch('/api/client', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(clientData),
+  });
+};
+
+// Account
+export const getAllAccounts = () => {
+  return fetch('/api/account', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const getAccount = (accountId) => {
+  return fetch(`/api/client/${accountId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const createAccount = (accountData) => {
+  return fetch('/api/client', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(accountData),
+  });
+};
+
+// Teller
+export const getAllTellers = () => {
+  return fetch('/api/teller', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const getTeller = (tellerId) => {
+  return fetch(`/api/matchup/${tellerId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export const createTeller = (tellerData) => {
   return fetch('/api/matchup', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(matchupData),
-  });
-};
-
-export const getMatchup = (matchupId) => {
-  return fetch(`/api/matchup/${matchupId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
-
-export const createVote = (voteData) => {
-  return fetch(`/api/matchup/${voteData}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(voteData),
-  });
-};
-
-export const getAllTech = () => {
-  return fetch('/api/tech', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    body: JSON.stringify(tellerData),
   });
 };
