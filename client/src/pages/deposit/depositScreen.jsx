ecc8e744606359fced9003fe5cf75ae043a4ab2e
francis <clyderitchie@yahoo.com>
2024-01-28 21:49:21 -0800

    Still working on a layout for the Deposit screen

--- client/src/pages/deposit/depositScreen.jsx
+++ client/src/pages/deposit/depositScreen.jsx
@@ -3,7 +3,44 @@ import './depositScreen.css'
 function Deposit () {
     return (
         <>
-            <h1>Deposit screen</h1>
+            <div className="container mt-5 border">
+                <div className="row">
+                    <div id="accountInfo" className="col-12">
+                        <h1 id="accountInfoTitle">Account Info</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                    <div className="accountInfoData">
+                            <p>Current Balance:</p>
+                            <p>Available Balance:</p>
+                            <p>Ledger Balance:</p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="accountInfoData">
+                            <p>Account Status:</p>
+                            <p>Account Owners:</p>
+                            <p>Date Opened:</p>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <div className="accountInfoData">
+                            <p>Holds:</p>
+                            <p>Return Items:</p>
+                            <p>Notes:</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mt-3 border">
+                <div className="row">
+                    <div className="col-12">
+                        Deposit section
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
