399ec273946ada17c8a2fd468f382399d5d03628
francis <clyderitchie@yahoo.com>
2024-01-28 20:17:41 -0800

    Styled the client single view page to a nice point

--- client/src/pages/singleClientView/singleclient.jsx
+++ client/src/pages/singleClientView/singleclient.jsx
@@ -5,25 +5,23 @@ function ClientView() {
 
     return (
         <>
-            <h1>SingleClient</h1>
-
             <div className="container mt-5 border">
                 <div className="row">
                     <div id="personalCol" className="col-6">
-                        <h2 id="clientName">Name: (Client name here)</h2>
+                        <h1 id="clientName">Name: (Client name here)</h1>
                         <div id="personalInfo">
                             <h3>Personal Info</h3>
                             <div id="info">
-                            <p>Address:</p>
-                            <p>Email:</p>
-                            <p>Phone Number:</p>
-                            <p>SSN:</p>
-                            <p>ID:</p>
+                                <p>Address:</p>
+                                <p>Email:</p>
+                                <p>Phone Number:</p>
+                                <p>SSN:</p>
+                                <p>ID:</p>
                             </div>
                         </div>
                     </div>
                     <div id="accountCol" className="col-6">
-                        <h2>Account Info</h2>
+                        <h1 id="clientAcccounts">Account Info</h1>
                         <div id="accountDrop" className="dropdown">
                             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Accounts
@@ -34,21 +32,15 @@ function ClientView() {
                                 <li><a className="dropdown-item" href="#">Money Market</a></li>
                                 <li><a className="dropdown-item" href="#">CD</a></li>
                             </ul>
+                            <h3 id="account">(Account name and number here)</h3>
                         </div>
-                        <div id="accountInfo">
-                            <h4>Available Balance:</h4>
-                            <h4>Current Balance:</h4>
-                            <h4>Date Opened:</h4>
-                            <div className="dropdown">
-                            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Account Details
-                            </button>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Owners: </a></li>
-                                <li><a className="dropdown-item" href="#">Total NSF:</a></li>
-                                <li><a className="dropdown-item" href="#">YTD NSF:</a></li>
-                            </ul>
-                        </div>
+                        <div id="balance">
+                            <p>Current Balance:</p>
+                            <p>Available Balance:</p>
+                            <p>Ledger Balance:</p>
+                            <p>Date Opened:</p>
+                            <p>YTD NSF:</p>
+                            <p>Total NSF:</p>
                         </div>
                     </div>
                 </div>
